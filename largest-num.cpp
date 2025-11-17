3d0527489df380a32e1a2d1b178709ce04667f57
Harsh Kumar Sharma <kumarsharmah01@gmail.com>
2025-11-17 22:09:37 +0530

    Add largest number finding functionality
    Implement a function to find the largest number in an array.

---
+++ largest-num.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int largest(int arr[],int n)
+{
+    int lar=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>lar){
+            lar=arr[i];
+        }
+    }// function for finding largest number in array
+    cout<<"Largest element is: "<<lar;
+    return lar;
+}
+int main(){
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    largest(arr,n);// calling function largest
+    return 0;
+}
